36df0f8ebbd09375be50b5b82c711cc8cdfa01a0
coldstar <safeie@163.com>
2019-04-13 08:14:43 +0800

    add more log output method

--- accesslog.go
+++ accesslog.go
@@ -2,9 +2,13 @@
 package accesslog
 
 import (
+	"fmt"
+	"io"
+	"log"
+	"os"
 	"time"
 
-	"gopkg.in/baa.v1"
+	baa "gopkg.in/baa.v1"
 )
 
 // Logger returns a baa middleware for log http access
@@ -14,6 +18,26 @@ func Logger() baa.HandlerFunc {
 
 		c.Next()
 
-		c.Baa().Logger().Printf("%s %s %s %v %v", c.RemoteAddr(), c.Req.Method, c.URL(true), c.Resp.Status(), time.Since(start))
+		c.Baa().Logger().Printf("%s %s %s %v %v\n", c.RemoteAddr(), c.Req.Method, c.URL(true), c.Resp.Status(), time.Since(start))
 	}
+}
+
+// LoggerWithWriter returns a baa middleware with writer for log http access
+func LoggerWithWriter(f io.Writer) baa.HandlerFunc {
+	return func(c *baa.Context) {
+		start := time.Now()
+
+		c.Next()
+
+		fmt.Fprintf(f, "%s %s %s %v %v\n", c.RemoteAddr(), c.Req.Method, c.URL(true), c.Resp.Status(), time.Since(start))
+	}
+}
+
+// LoggerWithFile returns a baa middleware with file write for log http access
+func LoggerWithFile(file string) baa.HandlerFunc {
+	f, err := os.OpenFile(file, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		log.Fatalf("accesslog.LoggerWithFile: open logfile [%s] err: %v", file, err)
+	}
+	return LoggerWithWriter(f)
 }
